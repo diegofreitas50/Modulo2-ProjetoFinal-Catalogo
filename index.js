2b43f0553ab3780bb476def037f934936759b602
diegofreitas50 <diegofreitas50@hotmail.com>
2022-03-29 23:54:56 -0300

    estilização

--- index.js
+++ index.js
@@ -14,5 +14,13 @@ app.get("/index", (req, res) => {
     res.render("index"); // Nome do arquivo, o EJS já busca dentro da pasta views.
 });
 
+app.get("/cadastro", (req, res) =>{
+    res.render("cadastro")
+});
+
+app.get("/contatos", (req, res) =>{
+    res.render("contatos")
+});
+
 // Adicionando a const port e uma arow function de callback para mostrar no console que o servidor está rodando.
 app.listen(port, () => console.log(`Servidor rodando em http://localhost:${port}`));
