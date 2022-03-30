c2dac9643e0bcf5b083a9b81b56355b7424541d7
diegofreitas50 <diegofreitas50@hotmail.com>
2022-03-29 23:57:24 -0300

    procfile

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000; // Const para armanezar a porta do servidor
+const port = process.env.PORT || 3000; // Const para armanezar a porta do servidor
 const path = require("path");
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
