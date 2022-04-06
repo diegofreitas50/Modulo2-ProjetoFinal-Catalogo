c17c5e10a6d1de06afae60c6c0736ceaf176b5bb
diegofreitas50 <diegofreitas50@hotmail.com>
2022-04-06 18:58:48 -0300

    up

--- src/database/db.js
+++ src/database/db.js
@@ -1,13 +1,14 @@
 const Sequelize = require("sequelize");
 const connect = new Sequelize(
-  process.env.DB_BASE,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: 5432,
+  process.env.DATABASE_URL, {
     dialect: "postgres",
+    dialectOptions:{
+      ssl:{
+        require:true,
+        rejectUnauthorized:false,
+      }
+    }
   }
 );
 
-module.exports = connect;
+module.exports = connect; 
